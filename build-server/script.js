b56f62526c64942dfcc5f0c84c9d871bb48b589d
Chandra Kumar Rajwal <122195878+imckr@users.noreply.github.com>
2024-09-12 23:20:03 +0530

    initialized

---
+++ build-server/script.js
@@ -0,0 +1,51 @@
+const { exec } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3")
+const mime = require("mime-types");
+
+const s3Client = new S3Client({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: "AKIA5FTZDQVCIGVVFU64",
+    secretAccessKey: "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",
+  },
+});
+
+const PROJECT_ID = process.env.PROJECT_ID;
+
+async function init() {
+  console.log("Executing Script.js");
+
+  const outDirPath = path.join(__dirname, "output");
+
+  const p = exec(`cd ${outDirPath} && npm install && npm run build`);
+
+  p.stdout.on("data", function (data) {
+    console.log(data.toString());
+  });
+
+  p.stdout.on("error", function (data) {
+    console.log("Error: " + data.toString());
+  });
+
+  p.on("close", async function () {
+    console.log("Build Completed");
+    const distFolderPath = path.join(__dirname, "output", "dist");
+    const distFolderContents = fs.readdirSync(distFolderPath, {
+      recursive: true,
+    });
+    for (const filePath of distFolderContents) {
+      if (fs.lstatSync(filePath).isDirectory()) continue;
+      
+      const command = new PutObjectCommand({
+        Bucket: 'inspire.deployes',
+        key: `__outputs/${PROJECT_ID}/${filePath}`,
+        Body: fs.createReadStream(filePath),
+        ContentType: mime.lookup(filePath)
+      })
+      await s3Client.send(command)
+    }
+    console.log("Deployment to S3 completed");
+  });
+}
